04b2b9be41a09937ab27d2dfc7976d016442bbdd
Danilocb25 <danilocoronel96@gmail.com>
2024-03-05 10:20:11 -0500

    update imaages

--- src/blackjack/usecases/add-img-carta.js
+++ src/blackjack/usecases/add-img-carta.js
@@ -3,8 +3,9 @@ import { divCartasJugadores } from "..";
 export const insertaImagen = ( carta, turno ) => {
 
     const imgCarta = document.createElement('img');
-    imgCarta.src = `assets/cartas/${carta}.png`;
+    imgCarta.src = `public/assets/cartas/${carta}.png`;
     imgCarta.classList.add('carta');
     divCartasJugadores[turno].append(imgCarta);
 
   };
+  //./public/assets/cartas/${carta}.png
